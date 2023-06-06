0f3f2ceea319efff09c4322e88ba19123eadebdb
Guilherme Carvalho <guilherme.carvalhos@outlook.com>
2023-06-06 00:27:36 -0300

    alinhamento gerais

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,18 +4,13 @@ import { SobreComponent } from './sobre/sobre.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {
-    path: 'sobre',
-    component: SobreComponent
-  },
-  {
-    path: 'home',
-    component: HomeComponent
-  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'sobre', component: SobreComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
